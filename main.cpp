f1e60d2717f277023ee53fac508f08e4bdf0b571
Jonhi7139 <125214537+Jonhi7139@users.noreply.github.com>
2026-01-19 15:12:36 +0000

    First walking code

--- main.cpp
+++ main.cpp
@@ -10,11 +10,35 @@
 // Limites típicos de servo (ajusta depois!)
 #define SERVO_MIN  150  // pulso mínimo
 #define SERVO_MAX  600  // pulso máximo
+#define posmin 40
+#define posmax 140
+
+#define zerope 45
+#define doispe 55
+#define quatrope 65
+#define oitope 25
+#define dezpe 50
+#define dozepe 30
+
+#define umpe 120
+#define trespe 90
+#define cincope 65
+#define novepe 115
+#define onzepe 80
+#define trezepe 90
+
+
+
+
+
 int pos;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(PCA9685_ADDR);
 
 // Forward declaration
 void moveServo(uint8_t channel, uint8_t angle);
+void Levanta_Aranha();
+void Deita_Aranha();
+void UmPasso();
 
 void setup() {
   Serial.begin(115200);
@@ -37,41 +61,42 @@ void setup() {
 
   // Teste: mover servo do canal 0 para posição central
   pos=0;
-  moveServo(0,90);
-  moveServo(1,90);
-  moveServo(2,90);
-  moveServo(3,90);
-  moveServo(4,90);
-  moveServo(5,90);
+  moveServo(0,45);        // CALIBRADA EM PÉ -> 45      Levantada para mover -> 90
+  moveServo(1,120);       // CALIBRADA EM PÉ -> 120     andar para mover -> 80
+  moveServo(2,55);        // CALIBRADA EM PÉ -> 55      Levantada para mover -> 
+  moveServo(3,90);        // CALIBRADA EM PÉ -> 90
+  moveServo(4,65);        // CALIBRADA EM PÉ -> 65      Levantada para mover -> 105
+  moveServo(5,65);        // CALIBRADA EM PÉ -> 65      andar para mover -> 105
   moveServo(6,90);
   moveServo(7,90);
-  moveServo(8,90);
-  moveServo(9,90);
-  moveServo(10,90);
-  moveServo(11,90);
-  moveServo(12,90);
-  delay(1000);
+  moveServo(8,25);        // CALIBRADA  EM PÉ -> 25     Levantada para mover ->
+  moveServo(9,115);       // CALIBRADA EM PÉ -> 115
+  moveServo(10,50);       // CALIBRADA EM PÉ -> 50      Levantada para mover ->95
+  moveServo(11,80);       // CALIBRADA EM PÉ -> 80       andar para mover -> 120
+  moveServo(12,50);       // CALIBRADA EM PÉ -> 30      Levantada para mover ->
+  moveServo(13,90);       // CALIBRADA EM PÉ -> 90
+  moveServo(14,90); 
+  moveServo(15,90);
+  delay(3000);
 }
 
 void loop() {
-  for (pos=90; pos<=120; pos++){
-    moveServo(7, pos);
-    moveServo(12, pos);
-    moveServo(0,pos);
-    moveServo(1,pos);
-    delay(5);
-  }
+
+
+
+         /* ROTINA 
+  Deita_Aranha();
   delay(2000);
   
-  for (pos=120; pos>=90; pos--){
-    moveServo(7, pos);
-    moveServo(12, pos);
-    moveServo(0,pos);
-    moveServo(1,pos);
-    delay(5);
-  }
-  delay(2000);
+  Levanta_Aranha();
+  delay(2000);*/
+           
+  UmPasso();
 }
+
+
+
+
 
 // Função para mover servo por ângulo (0–180)
 void moveServo(uint8_t channel, uint8_t angle) {
@@ -85,4 +110,80 @@ void moveServo(uint8_t channel, uint8_t angle) {
   Serial.print(" -> ");
   Serial.print(angle);
   Serial.println(" graus");
+}
+
+void Levanta_Aranha(){
+  for (pos=0; pos<=50; pos=pos+2){
+    moveServo(0,zerope+50-pos);
+    moveServo(2,doispe+50-pos);
+    moveServo(4,quatrope+50-pos);
+    moveServo(8,oitope+50-pos);
+    moveServo(10,dezpe+50-pos);
+    moveServo(12,dozepe+50-pos);
+    delay(50);
+  }
+}
+
+void Deita_Aranha(){
+  for (pos=0; pos<=50; pos=pos+2){
+    moveServo(0,zerope+pos);
+    moveServo(2,doispe+pos);
+    moveServo(4,quatrope+pos);
+    moveServo(8,oitope+pos);
+    moveServo(10,dezpe+pos);
+    moveServo(12,dozepe+pos);
+    delay(50);
+  }
+}
+void UmPasso()
+{
+  for (pos=0; pos<=40; pos=pos+2){//sobem A
+    moveServo(0,zerope+pos);
+    moveServo(4,quatrope+pos);
+    moveServo(10,dezpe+pos);
+    delay(15);
+  }
+    delay(30);
+
+  for (pos=0; pos<=30; pos=pos+2){//mexem A1
+    moveServo(1,umpe-pos);
+    moveServo(5,cincope-pos);
+    moveServo(11,onzepe+pos);
+    delay(15);
+  }
+    delay(30);
+
+
+  for (pos=0; pos<=40; pos=pos+2){//descem A
+    moveServo(0,zerope+40-pos);
+    moveServo(4,quatrope+40-pos);
+    moveServo(10,dezpe+40-pos);
+    delay(15);
+  }
+    delay(30);
+
+
+  for (pos=0; pos<=40; pos=pos+2){//sobem B
+    moveServo(2,doispe+pos);
+    moveServo(8,oitope+pos);
+    moveServo(12,dozepe+pos);
+    delay(15);
+  }
+  delay(30);
+for (pos=0; pos<=30; pos=pos+2){//andam A1
+    moveServo(1,umpe-30+pos);
+    moveServo(5,cincope-30+pos);
+    moveServo(11,onzepe+30-pos);
+    delay(15);
+  }
+    delay(30);
+
+    for (pos=0; pos<=40; pos=pos+2){//descem B
+    moveServo(2,doispe+40-pos);
+    moveServo(8,oitope+40-pos);
+    moveServo(12,dozepe+40-pos);
+    delay(15);
+  }
+  delay(30);
+
 }
