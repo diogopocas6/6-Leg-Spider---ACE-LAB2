6732bdb0535a51e1523f0da127b76d60f376c413
diogopocas6 <81244186+diogopocas6@users.noreply.github.com>
2026-01-20 15:06:49 +0000

    Refactor spider movement functions with speed control
    Updated the speed parameters for the spider's movements and added new states for speed selection. Modified functions to accept speed as an argument for movement actions.

--- codigoPocas.cpp
+++ codigoPocas.cpp
@@ -13,7 +13,7 @@
 #define posmin 40
 #define posmax 140
 
-#define zerope 45
+#define zerope 40
 #define umpe 120
 #define doispe 55
 #define trespe 90
@@ -40,23 +40,29 @@ fsm_t fsm;
 
 //Estados fsm
 enum {
-  Aranha_deitada = 0,     // estado inicial
-  Aranha_a_levantar,     // executa Levanta_Aranha()
-  Aranha_em_pe,          // estado estável
-  Aranha_a_deitar,       // executa Deita_Aranha()
-  Aranha_a_andar,          // (futuro)
+  Aranha_deitada = 0,           // estado inicial
+  Aranha_a_levantar,            // executa Levanta_Aranha()
+  Aranha_em_pe,                 // estado estável
+  Aranha_a_deitar,              // executa Deita_Aranha()
+  Aranha_a_andar,               // Executa Walk()
+  Aranha_velocidade_levantar,   // estado para escolher velocidade da rotina Levanta_Aranha()
+  Aranha_velocidade_deitar,      // estado para escolher velocidade da rotina Deita_Aranha()
+  Aranha_velocidade_andar       // estado para escolher velocidade da rotina Walk()
 };
 
 // Forward declaration
 void moveServo(uint8_t channel, uint8_t angle);
-void Levanta_Aranha();
-void Deita_Aranha();
+void Levanta_Aranha(int velocidade);
+void Deita_Aranha(int velocidade);
+void Walk(int velocidade);
 void fsm_update();
 void set_state(fsm_t &fsm, int new_state);
 
 // Bloco para utilizarmos o Serial para transição de estados
 char cmd = 0;
-
+int speed_l = 1;
+int speed_d = 1;
+int speed_w = 1;
 void read_serial() {
   if (Serial.available()) {
     cmd = Serial.read();
@@ -64,6 +70,7 @@ void read_serial() {
     cmd = 0;
   }
 }
+//
 
 void setup() {
   Serial.begin(115200);
@@ -110,60 +117,6 @@ void loop() {
 
   read_serial();
   fsm_update();
-
-
-/* 
-  
-  // Passo para a frente
-  //Levanta primeiro as patas 0 4 e 10 amplitude 45º
-  for (pos=0;pos<45;pos++){
-    moveServo(0,zerope+pos);
-    moveServo(4,quatrope+pos);
-    moveServo(10,dezpe+pos);
-    delay(5);
-  }
-  delay(3000);
-
-  /*moveServo(0,zerope);
-  moveServo(4,quatrope);
-  moveServo(10,dezpe);
-  delay(5000);
-  
-  //Arrasta as patas do chao para a frente (servos de cima), mover 3 9 13 para a frente/trás amplitude 30º
-  for (pos=0;pos<30;pos++){
-    moveServo(3,90+pos);
-    moveServo(9,115-pos);
-    moveServo(13,90-pos);
-    delay(5);
-  }
-  //coloca patas do ar no chao () logo a seguir a arrastar as patas
-  for (pos=0;pos<45;pos++){
-    moveServo(0,zerope+45-pos);
-    moveServo(4,quatrope+45-pos);
-    moveServo(10,dezpe+45-pos);
-    delay(1);
-  }
-  delay(5000);
-
-  // posiçao inicial
-  moveServo(3,trespe);
-  moveServo(9,novepe);
-  moveServo(13,trezepe);
-  moveServo(0,zerope);
-  moveServo(4,quatrope);
-  moveServo(10,dezpe);
-  delay(5000);
-    */
-
-
-   /* ROTINA DEITA E LEVANTA
-  Deita_Aranha();
-  delay(2000);
-  
-  Levanta_Aranha();
-  delay(2000);
-           */
-
 }
 
 
@@ -177,35 +130,199 @@ void moveServo(uint8_t channel, uint8_t angle) {
   uint16_t pulse = map(angle, 0, 180, SERVO_MIN, SERVO_MAX);
   pwm.setPWM(channel, 0, pulse);
 
+  /*
   Serial.print("Servo ");
   Serial.print(channel);
   Serial.print(" -> ");
   Serial.print(angle);
   Serial.println(" graus");
+  */
 }
 
-void Levanta_Aranha(){
-  for (pos=0; pos<=50; pos=pos+2){
+void Levanta_Aranha(int velocidade){
+  int delay_number, incremento;
+  if (velocidade == 1){
+    delay_number=50;
+    incremento = 1;
+  }
+  if (velocidade == 2){
+    delay_number = 25;
+    incremento = 2;
+  }
+  if (velocidade == 3){
+    delay_number = 25;
+    incremento = 5;
+  }
+
+  for (pos=0; pos<=50; pos=pos+incremento){
     moveServo(0,zerope+50-pos);
     moveServo(2,doispe+50-pos);
     moveServo(4,quatrope+50-pos);
     moveServo(8,oitope+50-pos);
     moveServo(10,dezpe+50-pos);
     moveServo(12,dozepe+50-pos);
-    delay(50);
+    delay(delay_number);
   }
 }
 
-void Deita_Aranha(){
-  for (pos=0; pos<=50; pos=pos+2){
+void Deita_Aranha(int velocidade){
+  int delay_number, incremento;
+  if (velocidade == 1){
+    delay_number=50;
+    incremento = 1;
+  }
+  if (velocidade == 2){
+    delay_number = 25;
+    incremento = 2;
+  }
+  if (velocidade == 3){
+    delay_number = 25;
+    incremento = 5;
+  }
+  for (pos=0; pos<=50; pos=pos+incremento){
     moveServo(0,zerope+pos);
     moveServo(2,doispe+pos);
     moveServo(4,quatrope+pos);
     moveServo(8,oitope+pos);
     moveServo(10,dezpe+pos);
     moveServo(12,dozepe+pos);
-    delay(50);
+    delay(delay_number);
   }
+
+}
+//Função que faz robô andar
+void Walk(int velocidade){
+int intervalo, delay_servos,delay_passos,incremento,pos_interval;
+if (velocidade == 1){
+    delay_servos = 15;
+    incremento = 1;
+    pos_interval = 30;
+    delay_passos = 200;
+  }
+  if (velocidade == 2){
+    delay_servos = 15;
+    incremento = 1;
+    pos_interval = 30;
+    delay_passos = 200;
+  }
+  if (velocidade == 3){
+    delay_servos = 15;
+    incremento = 1;
+    pos_interval = 30;
+    delay_passos = 200;
+  }
+
+  //Serial.println("A - Passo 1");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){    //sobem A
+    moveServo(0,zerope+pos);
+    moveServo(4,quatrope+pos);
+    moveServo(10,dezpe+pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("A - Passo 2");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){    //mexem A1
+    moveServo(1,umpe-pos);
+    moveServo(5,cincope-pos);
+    moveServo(11,onzepe+pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("A - Passo 3");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//descem A
+    moveServo(0,zerope+pos_interval-pos);
+    moveServo(4,quatrope+pos_interval-pos);
+    moveServo(10,dezpe+pos_interval-pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("A - Passo 4 ");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//sobem B
+    moveServo(2,doispe+pos);
+    moveServo(8,oitope+pos);
+    moveServo(12,dozepe+pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("A - Passo 5");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//andam A1
+    moveServo(1,umpe-pos_interval+pos);
+    moveServo(5,cincope-pos_interval+pos);
+    moveServo(11,onzepe+pos_interval-pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("A - Passo 6");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//descem B
+    moveServo(2,doispe+pos_interval-pos);
+    moveServo(8,oitope+pos_interval-pos);
+    moveServo(12,dozepe+pos_interval-pos);
+    delay(delay_servos);
+  } 
+
+  delay(delay_passos); 
+  
+  //------------------------------------------------//
+  // SEGUNDA PARTE DO MOVIMENTO PARA SER DOIS PASSOS//
+  //------------------------------------------------//
+  //Serial.println("B - Passo 1");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//sobem B
+    moveServo(2,doispe+pos);
+    moveServo(8,oitope+pos);
+    moveServo(12,dozepe+pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("B - Passo 2");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//mexem B1
+    moveServo(3,trespe-pos);
+    moveServo(9,novepe-pos);
+    moveServo(13,trezepe+pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("B - Passo 3");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//descem B
+    moveServo(2,doispe+pos_interval-pos);
+    moveServo(8,oitope+pos_interval-pos);
+    moveServo(12,dozepe+pos_interval-pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("B - Passo 4");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//sobem A
+    moveServo(0,zerope+pos);
+    moveServo(4,quatrope+pos);
+    moveServo(10,dezpe+pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("B - Passo 5");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//andam B1
+    moveServo(3,trespe-pos_interval+pos);
+    moveServo(9,novepe-pos_interval+pos);
+    moveServo(13,trezepe+pos_interval-pos);
+    delay(delay_servos);
+  }
+  delay(delay_passos);
+
+  //Serial.println("B - Passo 6");
+  for (pos=0; pos<=pos_interval; pos=pos+incremento){//descem A
+    moveServo(0,zerope+pos_interval-pos);
+    moveServo(4,quatrope+pos_interval-pos);
+    moveServo(10,dezpe+pos_interval-pos);
+    delay(delay_servos);
+  } 
+  delay(delay_passos);
 
 }
 
@@ -217,29 +334,83 @@ void fsm_update() {
 
     case Aranha_deitada:
       if (cmd == 'l') {     // l no serial monitor faz com que a aranha levante
-        set_state(fsm, Aranha_a_levantar);
+        set_state(fsm,Aranha_velocidade_levantar);
+        Serial.println("Escolha a velocidade para levantar entre 1 e 3.");
+        //set_state(fsm, Aranha_a_levantar);
+      }
+      break;
+
+    case Aranha_velocidade_levantar:
+      if (cmd != 0) {
+        if (cmd == '1' || cmd == '2' || cmd == '3') {
+          speed_l = cmd - '0';    // se fizessemos simplesmente (int)cmd , o valor não seria 1, por causa da conversão hexadecimal!
+          set_state(fsm,Aranha_a_levantar);
+        }
+        else {
+          Serial.println("Velocidade inválida!Escolha uma velocidade entre 1 e 3.");
+        }
       }
       break;
 
     case Aranha_a_levantar:
-      Levanta_Aranha();
+      Levanta_Aranha(speed_l);
+      speed_l=1;
       set_state(fsm, Aranha_em_pe);
+      break;
+
+    case Aranha_velocidade_deitar:
+      if (cmd != 0) {
+        if (cmd == '1' || cmd == '2' || cmd == '3') {
+          speed_d = cmd - '0';    // se fizessemos simplesmente (int)cmd , o valor não seria 1, por causa da conversão hexadecimal!
+          set_state(fsm,Aranha_a_deitar);
+        }
+        else {
+          Serial.println("Velocidade inválida!Escolha uma velocidade entre 1 e 3.");
+        }
+      }
+      break;
+
+    case Aranha_velocidade_andar:
+      if (cmd != 0) {
+        if (cmd == '1' || cmd == '2' || cmd == '3') {
+          speed_d = cmd - '0';    // se fizessemos simplesmente (int)cmd , o valor não seria 1, por causa da conversão hexadecimal!
+          set_state(fsm,Aranha_a_andar);
+          Serial.println("Para parar o robô, utilize a tecla 's'.");
+        }
+        else {
+          Serial.println("Velocidade inválida!Escolha uma velocidade entre 1 e 3.");
+        }
+      }
       break;
 
     case Aranha_em_pe:
       if (cmd == 'd') {     // d no serial monitor faz com que a aranha deite
-        set_state(fsm, Aranha_a_deitar);
+        set_state(fsm, Aranha_velocidade_deitar);
+        Serial.println("Escolha a velocidade para levantar entre 1 e 3.");
+      }
+      if (cmd == 'w') {     // w no serial monitor faz com que a aranha ande
+        set_state(fsm,Aranha_velocidade_andar);
+        Serial.println("Escolha a velocidade para andar entre 1 e 3.");
       }
       break;
-
+    
     case Aranha_a_deitar:
-      Deita_Aranha();
+      Deita_Aranha(speed_d);
+      speed_d=1;
       set_state(fsm, Aranha_deitada);
       break;
 
     case Aranha_a_andar:
-      // futuro
+    if (cmd == 's') {
+      set_state(fsm,Aranha_em_pe);
+      Serial.println("Aranha em pé. Comandos disponíveis:");
+      Serial.println("Andar - 'w'");
+      Serial.println("Deitar - 'd'");
+    }
+    else {
+      Walk(speed_w);
       break;
+    }
   }
 }
 
